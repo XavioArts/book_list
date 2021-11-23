2aa0a937ccf73fbbee28b9e88ae046b3332da5f8
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-23 16:57:25 -0700

    view all books working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Books from "./Books";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Book List</h1>
+        <Books />
       </header>
     </div>
   );
--- src/Books.js
+++ src/Books.js
@@ -2,7 +2,38 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Books = (props) => {
+    const [books, setBooks] = useState([]);
 
+    useEffect(() => {
+        getBooks();
+    }, []);
+
+    const getBooks = async () => {
+        let result = await axios.get("https://fakerapi.it/api/v1/books?_quantity=5");
+        setBooks(result.data.data)
+    };
+
+    const renderBooks = () => {
+        if(books.length === 0)
+            return <p>No books available</p>
+        else
+            return books.map((book) => {
+                return (
+                    <div className="book-container">
+                        <h1>{book.title}</h1>
+                        <h2>By {book.author}</h2>
+                        <img src={book.image} />
+                        <p>{book.description}</p>
+                    </div>
+                );
+            });
+    };
+
+    return (
+        <div className="books-container">
+            {renderBooks()}
+        </div>
+    );
 };
 
 export default Books;
